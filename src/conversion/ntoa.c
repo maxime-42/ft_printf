fc3cd6d37239f7049cbbf051bd02f36ef978db87
Maxime KAYUMBA ONAKASONGO <mkayumba@e3r13p13.42.fr>
2020-01-14 15:06:32 +0100

    update

--- src/conversion/ntoa.c
+++ src/conversion/ntoa.c
@@ -6,7 +6,7 @@
 /*   By: mkayumba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 12:32:29 by mkayumba          #+#    #+#             */
-/*   Updated: 2020/01/13 19:07:59 by mkayumba         ###   ########.fr       */
+/*   Updated: 2020/01/14 15:03:59 by mkayumba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,59 @@ static void	lenght_max(t_info *info)
 	}
 }
 
-void	ntoa(t_info *info, unsigned long long value)
+char		*ft_ntoa(long long n, char *base, char *p)
 {
-	int		digit;
-	static	int	index = 0;
+	char	sign;
+	t_size length;
+	size_t size_base;
 
-	lenght_max(info);
-	if (value >= info->base)
+	sign =  '\0';
+	if (n < 0) 
+		signr = ? '-';
+	size_base = ft_strlen(base);
+	length = ft_lenght_nbr(n, size_base);
+	if (!p && !(p = malloc(sizeof(char) * (length + 1))))
+		return (0);
+	p[length] = '\0';
+	while (length)
 	{
-		index++;
-		ntoa(info, value / info->base);
+		if (n < 0)
+			p[--length] = -(base[(n % size_base)]);
+		else
+			p[--length] = (base[(n % size_base)]);
+		n = n / size_base;
 	}
-	digit = value % info->base;
-	if (digit < 10)
-		info->buf[info->lenght++] = digit + '0';
-	else if (info->flags & FLAGS_UPPERCASE)
-	{
-		//printf("\nMAJUSCULE\n");
-		//return ;
-		info->buf[info->lenght++] = digit + 'A'- 10;
-	}
-	else
-	{
-		//printf("\nminiscule\n");
-	info->buf[info->lenght++] = digit + 'a'- 10;
-	}
-/* 	if ((info->flags & FLAGS_APOSTROPHE)) */
-/* 		apostrophe(info, &index); */
+	if (sign)
+		p[length] = sign;
+	return (p);
 }
+
+/* void	ntoa(t_info *info, unsigned long long value) */
+/* { */
+/* 	int		digit; */
+/* 	static	int	index = 0; */
+
+/* 	lenght_max(info); */
+/* 	if (value >= info->base) */
+/* 	{ */
+/* 		index++; */
+/* 		ntoa(info, value / info->base); */
+/* 	} */
+/* 	digit = value % info->base; */
+/* 	if (digit < 10) */
+/* 		info->buf[info->lenght++] = digit + '0'; */
+/* 	else if (info->flags & FLAGS_UPPERCASE) */
+/* 	{ */
+/* 		//printf("\nMAJUSCULE\n"); */
+/* 		//return ; */
+/* 		info->buf[info->lenght++] = digit + 'A'- 10; */
+/* 	} */
+/* 	else */
+/* 	{ */
+/* 		//printf("\nminiscule\n"); */
+/* 	info->buf[info->lenght++] = digit + 'a'- 10; */
+/* 	} */
+/* /\* 	if ((info->flags & FLAGS_APOSTROPHE)) *\/ */
+/* /\* 		apostrophe(info, &index); *\/ */
+/* } */
+
